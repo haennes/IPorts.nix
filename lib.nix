00c13bbdb8793ddd3dc38e139437335632772b1c
haennes <hannes.hofmuth@gmail.com>
2024-09-26 14:48:21 +0200

    fmt lib

--- lib.nix
+++ lib.nix
@@ -1,11 +1,10 @@
-{lib, ...}:
+{ lib, ... }:
 let inherit (lib) mapAttrs fold recursiveUpdate;
-in
-{
-  mapAttrsFunc = f: set: (mapAttrs(name: value: f value) set);
+in {
+  mapAttrsFunc = f: set: (mapAttrs (name: value: f value) set);
   recursiveMergeNoOverride = listOfAttrsets:
-          fold (attrset: acc: recursiveUpdate attrset acc) { } #TODO fix to disallow same path
-          listOfAttrsets;
-
+    fold (attrset: acc: recursiveUpdate attrset acc)
+    { } # TODO fix to disallow same path
+    listOfAttrsets;
 
 }
